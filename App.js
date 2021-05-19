866895cb873871816859142b19c38fe3295d4563
Nabeel <nabeelbaig962@gmail.com>
2021-05-19 23:02:14 +0300

    Installed react navigator. Added Login and Navigator

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi!</Text>
+      <Text>Hi!!</Text>
       <StatusBar style="auto" />
     </View>
   );
