c3568f41188c48893253434b2212ef83d9089fd5
Nabeel <nabeelbaig962@gmail.com>
2021-06-03 21:45:05 +0300

    Removed unused code

--- components/plants.js
+++ components/plants.js
@@ -20,9 +20,10 @@ export default class plants extends Component {
 
                     <View style={{ flex: 1, flexDirection: 'column' }}>
 
-                        <Text>{this.props.plant.plantInfo.name}</Text>
+                        <Text style={{fontWeight: 'bold'}}>{this.props.plant.plantInfo.name}</Text>
                         <Text>Location: {this.props.plant.plantInfo.location}</Text>
                         <Text>Price: {this.props.plant.plantInfo.price}</Text>
+                        <Text>Availability: {this.props.plant.plantInfo.availability}</Text>
                         <Text>Seller: {this.props.plant.seller.name}</Text>
                         <Text>Contact: {this.props.plant.seller.phone}</Text>
                         
@@ -38,70 +39,14 @@ export default class plants extends Component {
         this.setState({ plantState: value })
     }
 
-    setOutput(value) {
-        this.setState({ output: value })
-    }
-
-    Output() {
-
-        if (this.state.plantState == 0) {
-            this.setOutput(<>
-            <View style={styles.singleplant}>
-                <Image 
-                    style={styles.plantimg}
-                    source={{ 
-                        uri: 'https://res.cloudinary.com/djpjb7io8/image/upload/v1621478764/cactus.jpg',
-                     }}
-                />
-                <View style={{ flex: 1, flexDirection: 'column'}}>
-
-                     <Text style={{ fontSize: 15, fontWeight: "bold"}}>About:</Text>
-                     <Text>{this.props.plant.plantInfo.name}</Text>
-                     <Text>Price: {this.props.plant.plantInfo.price}</Text>
-                     <Text>Location: {this.props.plant.plantInfo.location}</Text>
-                     <Text>Description: {this.props.plant.plantInfo.description}</Text>
-                     <Text>Availability: {this.props.plant.plantInfo.availability}</Text>
-                     <Text style={{ fontSize:15, fontWeight: "bold" }}>Seller:</Text>
-                     <Text>Name: {this.props.plant.seller.name}</Text>
-                     <Text>Phone: {this.props.plant.seller.phone}</Text>
-
-                </View>
-            </View>
-           </> )
-           this.setItemState(1)
-        }
-
-        else if (this.state.plantState == 1) {
-            this.setOutput (<>
-                <Views style={styles.singleplant}>
-                    <Image
-                        style={styles.plantimg}
-                        source={{ 
-                            uri: 'https://res.cloudinary.com/djpjb7io8/image/upload/v1621478764/cactus.jpg',
-                        }}
-                    />
-                    <View style={{ flex: 1, flexDirection: 'column' }}>
-
-                         <Text>{this.props.plant.plantInfo.name}</Text>
-                        <Text>Price: {this.props.plant.plantInfo.price}</Text>
-                        <Text>Location: {this.props.plant.plantInfo.location}</Text>
-                        <Text>Seller: {this.props.plant.seller.name}</Text>
-                    
-                    </View>
-                </Views>
-            </>)
-            this.plantState(0)
-        }
-
-    }
-
+        
     render() {
         return (
-            <TouchableHighlight onPress={() => this.Output()} >
+            <View  >
                 {
                     this.state.output
                 }
-            </TouchableHighlight>
+            </View>
         )
     }
 
